db1ab1c4627340f02c0d675c7e2ecd23ea6601e8
hamyqueso <hamyqueso@gmail.com>
2017-12-04 16:29:55 -0800

    changes to day 3 part 1 solution

--- day3_part1.java
+++ day3_part1.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class day3_part1{
     public static void main(String[] args) {
         int number = 26;
@@ -5,23 +7,32 @@ public class day3_part1{
         int square = count*count;
         int size = 1;
         int distance = 0;
+        int offset = 0;
 
         while ((number - square) > 0){
             count+=2;
             size+=2;
             square = count*count;
             distance++;
+            offset++;
         }
-        count -= 2;
-        square = count*count;
-        int array[][] = new int[size][size];
-        int center =((int) Math.floor(size/2)) - 1;
+
+        int center1 = (count - 1)/2;
+        //int current = count*count;
+        //int array[][] = new int[size][size];
+        //int center =((int) Math.floor(size/2)) - 1;
         
-        for (int i = -1; i < size; i++){ //fill in left side of array
+        /*for (int i = center+offset; i > -1; i--){ //fill in left side of array
             
-            array[size -1][center + i]
-        }
-        System.out.println(center);
+            array[i][size -1] = current;
+            if(current == number){
+                break;
+            }
+            current++;
+
+        } */
+
+        System.out.println(count);
 
     }
 }
