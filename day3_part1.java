6e0db9d0ce0b884b4e4af41b4c026774ca1edbfd
Jonathan Cheung <hamyqueso@gmail.com>
2017-12-04 18:58:02 -0800

    Day 3 part 1 solution

--- day3_part1.java
+++ day3_part1.java
@@ -2,22 +2,52 @@ import java.util.Arrays;
 
 public class day3_part1{
     public static void main(String[] args) {
-        int number = 26;
+        int number = 312051;
         int count = 1;
         int square = count*count;
         int size = 1;
         int distance = 0;
         int offset = 0;
+        if(number == 1){
+            distance = 0;
+        } else {
+            while ((number - square) > 0){
+                count+=2;
+                size+=2;
+                square = count*count;
+                distance++;
+                offset++;
+            }
 
-        while ((number - square) > 0){
-            count+=2;
-            size+=2;
             square = count*count;
-            distance++;
-            offset++;
-        }
 
-        int center1 = (count - 1)/2;
+            int center1 = square - (count - 1)/2;
+            int center2 = square - (count - 1)/2 - (size - 1);
+            int center3 = square - (count-1)/2 - 2*(size - 1);
+            int center4 = square - (count-1)/2 - 3*(size - 1);
+
+            int hdistance = Math.abs(number - center1);
+            if(Math.abs(number - center2) < hdistance){
+                hdistance = Math.abs(number - center2);
+            }
+            if(Math.abs(number - center3) < hdistance){
+                hdistance = Math.abs(number - center3);
+            }
+            if(Math.abs(number - center4) < hdistance){
+                hdistance = Math.abs(number - center4);
+            }
+
+            distance +=hdistance;
+            /*
+            System.out.println(center1);
+            System.out.println(center2);
+            System.out.println(center3);
+            System.out.println(center4);
+            */
+            
+
+        }
+        System.out.println(distance);
         //int current = count*count;
         //int array[][] = new int[size][size];
         //int center =((int) Math.floor(size/2)) - 1;
@@ -32,7 +62,6 @@ public class day3_part1{
 
         } */
 
-        System.out.println(count);
-
+       
     }
 }
